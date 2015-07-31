f976bce4e89c12c7c80f62d2332a48cf5f10fe38
Andrew Jones <andrew_jones@outlook.com>
2015-07-31 17:35:31 +0200

    Added jshint for linting

--- test/intergration-tests.js
+++ test/intergration-tests.js
@@ -1,4 +1,3 @@
-var expect = require('chai').expect;
 var request = require('supertest');
 var app = require('../app/app');
 
@@ -6,6 +5,6 @@ var app = require('../app/app');
 suite('Intergration Tests', function() {
   it('should return the hello world page', function() {
     var response = request(app).get('/');
-      response.expect('Content-Type', "text/html; charset=utf-8");
+      response.expect('Content-Type', 'text/html; charset=utf-8');
       response.expect(200);
-  })});
+  });});
